5da65349adc19633c0fc9d02b302ab97f6789f97
Toni Förster <toni.foerster@icloud.com>
2025-03-16 21:15:45 +0100

    Initial Commit

---
+++ CMD-Z/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  CMD-Z
+//
+//  Created by Toni Förster on 16.03.25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ CMD-Z/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CMD-Z
+//
+//  Created by Toni Förster on 16.03.25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
